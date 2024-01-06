2564fd2a171f30d90116496e289ae2dd2b30c762
patrynk <rynkiewiczp2102@gmail.com>
2024-01-06 18:11:55 -0500

    Squashed 'lib/cpptraj/' content from commit 1b624bd5
    git-subtree-dir: lib/cpptraj
    git-subtree-split: 1b624bd56bd8b2a4672238a77c750da2f7145213

---
+++ src/Action_Strip.h
@@ -0,0 +1,24 @@
+#ifndef INC_ACTION_STRIP_H
+#define INC_ACTION_STRIP_H
+#include "Action.h"
+#include "ActionTopWriter.h"
+/// Used to remove atoms from the state.
+class Action_Strip: public Action {
+  public:
+    Action_Strip();
+    DispatchObject* Alloc() const { return (DispatchObject*)new Action_Strip(); }
+    void Help() const;
+    ~Action_Strip();
+  private:
+    Action::RetType Init(ArgList&, ActionInit&, int);
+    Action::RetType Setup(ActionSetup&);
+    Action::RetType DoAction(int, ActionFrame&);
+    void Print() {}
+
+    Topology* newParm_;
+    DataSetList* masterDSL_;
+    Frame newFrame_;
+    AtomMask M1_;
+    ActionTopWriter topWriter_;
+};
+#endif
